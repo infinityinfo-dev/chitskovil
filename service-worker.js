2b067022962909e947d7901a6ae83b1f460fc799
infinityinfo-dev <itsforappdevelop@gmail.com>
2026-09-20 20:21:39 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'chit-ledger-v2';
+const CACHE_NAME = 'chit-ledger-v3';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
